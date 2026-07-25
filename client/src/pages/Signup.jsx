0fc37ae6815ff61c407e51f617fb94e7dc061914
Venuri <venurivethmini2005@gmail.com>
2026-07-25 19:33:05 +0530

    Finalized Signup page styling and functionality

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -19,40 +19,69 @@ function Signup() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-[#0f0d0b]">
+
         <div className="bg-[#24201D] w-full max-w-md p-8 rounded-2xl shadow-xl border border-stone-800">
-      <h1 className="text-3xl font-bold text-[#F5F1EA]">GradBridge Signup Page</h1>
-      <h2 className="text-xl text-center mt-2 text-[#B8B0A8]">Create Account</h2>
+
+      <h1 className="text-3xl font-bold text-[#F5F1EA] text-center" >GradBridge</h1>
+
+      <h2 className="text-xl text-center mt-2 text-[#B8B0A8]">
+        Student Mentorship & Alumni Network
+      </h2>
+
+      <p className="text-center mt-2 text-[#B8B0A8]">
+        Create Account
+        </p>
 
       <p className="mt-6 mb-2 text-stone-300">Select your role:</p>
 
       <div className="flex gap-3 mb-6">
 
-      <button className="flex-1 bg-[#7E8C54] hover:bg-[#8E9E84] text-white p-2 rounded-lg transition" onClick={() => setRole("alumni")}>
+      <button 
+        className={`flex-1 p-2 rounded-lg transition ${
+            role === "alumni"
+                ? "bg-[#7E8C54] text-white"
+                : "bg-[#312C28] text-[#B8B0A8]"
+        }`} 
+        onClick={() => setRole("alumni")}>
         Alumni
         </button>
-      <button className="flex-1 bg-[#7E8C54] hover:bg-[#8E9E84] text-white p-2 rounded-lg transition" onClick={() => setRole("mentor")}>
-        Mentor</button>
+
+      <button 
+        className={`flex-1 p-2 rounded-lg transition ${
+            role === "mentor"
+                ? "bg-[#7E8C54] text-white"
+                : "bg-[#312C28] text-[#B8B0A8]"
+        }`} 
+        onClick={() => setRole("mentor")}>
+            Mentor</button>
 
         </div>
 
       <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
         {role === "mentor" && (
             <>
-            <input className="bg-[#312C28] border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Name" onChange={handleChange}/>
+            <input className="bg-[#312C28] border border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Name" onChange={handleChange}/>
 
-            <input className="bg-[#312C28] border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="NIC" onChange={handleChange}/>
+            <input className="bg-[#312C28] border border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="NIC" onChange={handleChange}/>
 
-            <input className="bg-[#312C28] border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="University" onChange={handleChange}/>
+            <input className="bg-[#312C28] border border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="University" onChange={handleChange}/>
             </>
         )}
         
-        <input className="bg-[#312C28] border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Email" onChange={handleChange}/>
+        <input className="bg-[#312C28] border border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Email" onChange={handleChange}/>
 
-        <input className="bg-[#312C28] border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Password" type="password" onChange={handleChange} />
+        <input className="bg-[#312C28] border border-[#4A433E] text-stone-100 p-2 rounded-lg placeholder:text-stone-500" placeholder="Password" type="password" onChange={handleChange} />
 
         <button type="submit" className="bg-[#7E8C54] hover:bg-[#8E9E84] text-white p-3 rounded-lg mt-3 transition">
         Sign Up
         </button>
+
+        <p className="text-center text-[#B8B0A8] mt-4">
+        Already have an account?
+        <button type="button" className="text-[#7E8C54] ml-2">
+            Sign In
+        </button>
+        </p>
         </form>
 
         </div>

012d65d1a93306281f72d252d1ffc12650c1ac27
shashini03 <shashinimaleesha984@gmail.com>
2026-07-27 21:22:03 +0530

    added student search interface

---
+++ client/src/pages/StudentSearch.jsx
@@ -0,0 +1,16 @@
+function StudentSearch() {
+    return (
+        <div>
+            <h1>Search Students</h1>
+
+            <input
+                type="text"
+                placeholder="Enter student name"
+            />
+
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default StudentSearch;

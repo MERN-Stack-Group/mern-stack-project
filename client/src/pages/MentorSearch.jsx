f4cb215e6f2ff772adba3775d8661a8e640128fd
shashini03 <shashinimaleesha984@gmail.com>
2026-07-27 21:17:48 +0530

    added mentor search interface

---
+++ client/src/pages/MentorSearch.jsx
@@ -0,0 +1,16 @@
+function MentorSearch() {
+    return (
+        <div>
+            <h1>Search Mentors</h1>
+
+            <input
+                type="text"
+                placeholder="Enter mentor name"
+            />
+
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default MentorSearch;

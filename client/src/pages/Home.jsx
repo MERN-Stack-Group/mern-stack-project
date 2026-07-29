6bc9ad699a3899b247875c1d87a270c2da27c01c
Venuri <venurivethmini2005@gmail.com>
2026-07-29 11:52:39 +0530

    Initialized Home page structure

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to GradBridge</h1>
+            <p>
+                Connect with alumni, mentors, and opportunities.
+            </p>
+        </div>
+    );
+}
+
+export default Home;

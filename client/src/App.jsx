dee3692c12c8b2e90d595e0f08f901238fe897fa
Venuri <venurivethmini2005@gmail.com>
2026-07-30 19:24:18 +0530

    improved landing page-sign up page connectivity

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [role, setRole] = useState(null);
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route 
+        path="/" 
+        element={
+        <Home
+          isLoggedIn={isLoggedIn}
+          setIsLoggedIn={setIsLoggedIn}
+          role={role}
+          setRole={setRole}
+            />
+          } 
+        />
+
+        <Route 
+        path="/signup" 
+        element={
+            <Signup />
+        } />
+
+        <Route 
+        path="/signin" 
+        element={
+            <Signin 
+              setIsLoggedIn={setIsLoggedIn}
+              setRole={setRole}/>
+        } />
       </Routes>
     </BrowserRouter>
   );

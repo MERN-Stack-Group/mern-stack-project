0609383d3cd529a0dc450967066d0839409f907e
Venuri <venurivethmini2005@gmail.com>
2026-07-30 11:49:38 +0530

    Created Basic structure for Homepage

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,21 @@
+function Footer() {
+    return (
+        <footer>
+
+            <h3>
+                GradBridge
+            </h3>
+
+            <p>
+                Connecting students and alumni.
+            </p>
+
+            <p>
+                © 2026 GradBridge. All rights reserved.
+            </p>
+
+        </footer>
+    );
+}
+
+export default Footer;
